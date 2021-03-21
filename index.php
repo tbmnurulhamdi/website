a76fb707375193b7d139f19a2c030ac2f57a0a00
tbmnurulhamdi <tb.hamdi30@gmail.com>
2021-03-21 13:47:41 +0700

    memperbaiki admin

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<!-- CSS -->
 	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"> 
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> -->
+
 
 	<title>LOGIN</title>
   </head>
@@ -28,9 +28,9 @@
 		<!--login form start-->
 		<form class="login-form" action="/include/cek_login.php" method="post">
 			<h2>Login</h2>
-			<input type="text" name="username" value="" placeholder="Username" required>
+			<input type="text" name="username" value="" placeholder="Username" autofocus required>
 			<input type="password" name="password" value="" placeholder="Password" required>
-			<button type="submit" name="login">Login</button>
+			<button type="submit" name="login" value="LOGIN">Login</button>
 		</form>
 		<!--login form end-->
 	</div>
@@ -38,15 +38,5 @@
 
 		
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script> -->
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
-    -->
-  </body>
+</body>
 </html>
