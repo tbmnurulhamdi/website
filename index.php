2d1bf37008df849e97e5a4945cd30d2afceca1f2
tbmnurulhamdi <tb.hamdi30@gmail.com>
2021-03-21 12:55:29 +0700

    ubah login 2

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<!-- CSS -->
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"> 
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> -->
 
 	<title>LOGIN</title>
   </head>
@@ -21,39 +23,25 @@
 	?>
 
 
-	<div class="container-fluid" >
-
-		<div class="position-absolute top-50 start-50 translate-middle" >
-			<div class="p-5 row border border-info border-5">
-
-				<form action="/include/cek_login.php" method="post">
-					<div class="form-floating mb-3">
-						<input type="email" class="form-control border border-dark rounded-pill" id="floatingInput" placeholder="name@example.com">
-						<label for="floatingInput">Email address</label>
-					</div>
-					<div class="form-floating mb-3">
-						<input type="password" class="form-control border border-dark rounded-pill" id="floatingPassword" placeholder="Password">
-						<label for="floatingPassword">Password</label>
-					</div>
-					<div class="d-grid gap-2 col-6 mx-auto">
-						<button type="submit" class="btn btn-info bg-gradient rounded-pill">LOGIN</button>
-					</div>
-					</form>
-				
-
-				
-			</div>
-		</div>
-
-
+	<!--form area start-->
+	<div class="form">
+		<!--login form start-->
+		<form class="login-form" action="/include/cek_login.php" method="post">
+			<h2>Login</h2>
+			<input type="text" name="username" value="" placeholder="Username" required>
+			<input type="password" name="password" value="" placeholder="Password" required>
+			<button type="submit" name="login">Login</button>
+		</form>
+		<!--login form end-->
 	</div>
-	
+	<!--form area end-->
+
 		
 
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script> -->
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
