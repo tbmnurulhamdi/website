6075b38d81c65c799644e26cbdac4740561a6b06
tbmnurulhamdi <tb.hamdi30@gmail.com>
2021-03-21 10:35:08 +0700

    merubah tampilan login

--- index.php
+++ index.php
@@ -1,40 +1,64 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>LOGIN</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<?php 
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+	<title>LOGIN</title>
+  </head>
+  <body>
+
+  <?php 
 	if(isset($_GET['pesan'])){
 		if($_GET['pesan']=="gagal"){
 			echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
 		}
 	}
 	?>
- 
-	<div class="kotak_login">
-		<p class="tulisan_login">Silahkan login</p>
- 
-		<form action="/include/cek_login.php" method="post">
-			<label>Username</label>
-			<input type="text" name="username" class="form_login" placeholder="Username .." required="required">
- 
-			<label>Password</label>
-			<input type="password" name="password" class="form_login" placeholder="Password .." required="required">
- 
-			<input type="submit" class="tombol_login" value="LOGIN">
- 
-			<br/>
-			<br/>
-			<center>
-				<a class="link" href="https://www.malasngoding.com">kembali</a>
-			</center>
-		</form>
-		
+
+
+	<div class="container-fluid" >
+
+		<div class="position-absolute top-50 start-50 translate-middle" >
+			<div class="p-5 row border border-info border-5">
+
+				<form action="/include/cek_login.php" method="post">
+					<div class="form-floating mb-3">
+						<input type="email" class="form-control border border-dark rounded-pill" id="floatingInput" placeholder="name@example.com">
+						<label for="floatingInput">Email address</label>
+					</div>
+					<div class="form-floating mb-3">
+						<input type="password" class="form-control border border-dark rounded-pill" id="floatingPassword" placeholder="Password">
+						<label for="floatingPassword">Password</label>
+					</div>
+					<div class="d-grid gap-2 col-6 mx-auto">
+						<button type="submit" class="btn btn-info bg-gradient rounded-pill">LOGIN</button>
+					</div>
+					</form>
+				
+
+				
+			</div>
+		</div>
+
+
 	</div>
- 
- 
-</body>
+	
+		
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+  </body>
 </html>
